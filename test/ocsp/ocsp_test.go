23c1bd75c970da3bac0faf9896619828d646eab6
jporres1296 <jporres@ikerlan.es>
2022-06-07 09:20:34 +0200

    Fixed OCSP Tests

--- test/ocsp/ocsp_test.go
+++ test/ocsp/ocsp_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bytes"
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/base64"
 	"encoding/pem"
 	"flag"
@@ -40,14 +42,20 @@ func TestOCSP_Get(t *testing.T) {
 		logger = level.NewFilter(logger, level.AllowInfo())
 		logger = log.With(logger, "caller", log.DefaultCaller)
 	}
-	caClient, _ := client.LamassuCaClient(*certPath, *domain)
-	caName, _ := CreateCa(*domain, *certPath)
-	dms, _ := CreateDMS(*domain, *certPath, caName)
-	dev, _ := CreateDevice(*domain, *certPath, dms.Id)
-	_, csr, _ := utils.GenrateRandKey(dev.Id)
 
-	devCert, caCert, _ := caClient.SignCertificateRequest(context.Background(), caDTO.Pki, caName, csr, true, csr.Subject.CommonName)
-	ocspRequestBytes, err := ocsp.CreateRequest(devCert, caCert, &ocsp.RequestOptions{})
+	devClient, _ := client.LamassuDevClient(*certPath, *domain)
+
+	caName, caCert, _ := CreateCa(*domain, *certPath)
+	dms, _ := CreateDMS(*domain, *certPath, caName)
+	dmscert, _ := utils.ReadCert(dmsCertFile)
+	dmskey, _ := utils.ReadKey(dmsKeyFile)
+	servercert, _ := utils.ReadCertPool(*certPath)
+	dev, _ := CreateDevice(*domain, *certPath, dms.Id)
+
+	_, csr, _ := utils.GenrateRandKey(dev.Id)
+	devCert, _ := devClient.Enroll(context.Background(), csr, caName, dmscert, dmskey, servercert, *domain+"/api/devmanager")
+
+	ocspRequestBytes, err := ocsp.CreateRequest(devCert.Cert, caCert, &ocsp.RequestOptions{})
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -64,11 +72,11 @@ func TestOCSP_Get(t *testing.T) {
 		t.Run(fmt.Sprintf("Testing %s", tc.name), func(t *testing.T) {
 			encodedRequest := base64.StdEncoding.EncodeToString(ocspRequestBytes)
 			reqURL := "https://" + *domain + "/api/ocsp/" + encodedRequest
+			http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 			resp, err := http.Get(reqURL)
 			if err != nil {
-				if err.Error() != tc.err.Error() {
-					t.Errorf("Got result is %s; want %s", err, tc.err)
-				}
+				fmt.Printf("err: %v\n", err)
+				t.Fail()
 			} else {
 				if resp.StatusCode != http.StatusOK {
 					t.Errorf("Not receiving expected response")
@@ -95,14 +103,18 @@ func TestOCSP_Post(t *testing.T) {
 		logger = level.NewFilter(logger, level.AllowInfo())
 		logger = log.With(logger, "caller", log.DefaultCaller)
 	}
-	caClient, _ := client.LamassuCaClient(*certPath, *domain)
-	caName, _ := CreateCa(*domain, *certPath)
+	devClient, _ := client.LamassuDevClient(*certPath, *domain)
+
+	caName, caCert, _ := CreateCa(*domain, *certPath)
 	dms, _ := CreateDMS(*domain, *certPath, caName)
+	dmscert, _ := utils.ReadCert(dmsCertFile)
+	dmskey, _ := utils.ReadKey(dmsKeyFile)
+	servercert, _ := utils.ReadCertPool(*certPath)
 	dev, _ := CreateDevice(*domain, *certPath, dms.Id)
 	_, csr, _ := utils.GenrateRandKey(dev.Id)
+	devCert, _ := devClient.Enroll(context.Background(), csr, caName, dmscert, dmskey, servercert, *domain+"/api/devmanager")
 
-	devCert, caCert, _ := caClient.SignCertificateRequest(context.Background(), caDTO.Pki, caName, csr, true, csr.Subject.CommonName)
-	ocspRequestBytes, err := ocsp.CreateRequest(devCert, caCert, &ocsp.RequestOptions{})
+	ocspRequestBytes, err := ocsp.CreateRequest(devCert.Cert, caCert, &ocsp.RequestOptions{})
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -118,11 +130,11 @@ func TestOCSP_Post(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(fmt.Sprintf("Testing %s", tc.name), func(t *testing.T) {
 			reqURL := "https://" + *domain + "/api/ocsp/"
+			http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 			resp, err := http.Post(reqURL, "application/ocsp-request", bytes.NewReader(ocspRequestBytes))
 			if err != nil {
-				if err.Error() != tc.err.Error() {
-					t.Errorf("Got result is %s; want %s", err, tc.err)
-				}
+				fmt.Printf("err: %v\n", err)
+				t.Fail()
 			} else {
 				if resp.StatusCode != http.StatusOK {
 					t.Errorf("Not receiving expected response")
@@ -213,14 +225,17 @@ func CreateDMS(domain string, certPath string, caName string) (dmsDTO.DMS, error
 	return dms, err
 }
 
-func CreateCa(domain string, certPath string) (string, error) {
+func CreateCa(domain string, certPath string) (string, *x509.Certificate, error) {
 	caClient, err := client.LamassuCaClient(certPath, domain)
 	caName := goid.NewV4UUID().String()
-	ca, err := caClient.CreateCA(context.Background(), caDTO.Pki, caName, caDTO.PrivateKeyMetadata{KeyType: "rsa", KeyBits: 2048}, caDTO.Subject{CommonName: caName}, 365*time.Hour, 30*time.Hour)
+	ca, err := caClient.CreateCA(context.Background(), caDTO.Pki, caName, caDTO.PrivateKeyMetadata{KeyType: "RSA", KeyBits: 2048}, caDTO.Subject{CommonName: caName}, 365*time.Hour, 30*time.Hour)
 	if err != nil {
-		return "", err
+		return "", nil, err
 	}
-	return ca.Name, nil
+	data, _ := base64.StdEncoding.DecodeString(ca.CertContent.CerificateBase64)
+	block, _ := pem.Decode([]byte(data))
+	cert, _ := x509.ParseCertificate(block.Bytes)
+	return ca.Name, cert, nil
 
 }
 
@@ -230,7 +245,7 @@ func CreateDevice(domain string, certPath string, dmsId string) (dto.Device, err
 		return dto.Device{}, err
 	}
 
-	dev, err := devClient.CreateDevice(context.Background(), "testDevice", goid.NewNilUUID().String(), dmsId, "description", []string{"tag1"}, "", "")
+	dev, err := devClient.CreateDevice(context.Background(), "testDevice", goid.NewV4UUID().String(), dmsId, "description", []string{"tag1"}, "", "")
 	if err != nil {
 		return dto.Device{}, err
 	}
