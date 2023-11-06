f2ea535ee3ea2582c5c948a46970bc0ce71897c9
Saiz Haritz <hsaizsierra@gmail.com>
2023-11-06 08:39:47 +0000

    monolithic dev

--- cmd/monolithic/development/main.go
+++ cmd/monolithic/development/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -9,6 +10,8 @@ import (
 	"github.com/fatih/color"
 	"github.com/lamassuiot/lamassuiot/pkg/v3/config"
 	"github.com/lamassuiot/lamassuiot/pkg/v3/test/monolithic"
+	rabbitmq_test "github.com/lamassuiot/lamassuiot/pkg/v3/test/subsystems/async-messaging/rabbitmq"
+	awskmssm_test "github.com/lamassuiot/lamassuiot/pkg/v3/test/subsystems/cryptoengines/aws-kms-sm"
 	keyvaultkv2_test "github.com/lamassuiot/lamassuiot/pkg/v3/test/subsystems/cryptoengines/keyvaultkv2"
 	postgres_test "github.com/lamassuiot/lamassuiot/pkg/v3/test/subsystems/storage/postgres"
 )
@@ -21,26 +24,55 @@ ________  _______   ________      ___    ___      _________  ________          _
   \ \  \\  \\ \  \_|\ \ \  \_\\ \   \/  /  /            \ \  \ \ \  \\\  \       \ \  \___|\ \  \\ \  \ \  \ 
    \ \__\\ _\\ \_______\ \_______\__/  / /               \ \__\ \ \_______\       \ \__\    \ \__\\ \__\ \__\
     \|__|\|__|\|_______|\|_______|\___/ /                 \|__|  \|_______|        \|__|     \|__| \|__|\|__|
-                                 \|___|/                                                                     `
+                                 \|___|/                                                                     
+`
 
 func main() {
+	awsIoTManager := flag.Bool("awsiot", false, "enable AWS IoT Manager")
+	awsIoTManagerUser := flag.String("awsiot-keyid", "", "AWS IoT Manager AccessKeyID")
+	awsIoTManagerPass := flag.String("awsiot-keysecret", "", "AWS IoT Manager SecretAccessKey")
+	awsIoTManagerRegion := flag.String("awsiot-region", "eu-west-1", "AWS IoT Manager Region")
+	awsIoTManagerID := flag.String("awsiot-id", "", "AWS IoT Manager ConnectorID")
+	flag.Parse()
+
+	fmt.Println("===================== FLAGS ======================")
+
+	fmt.Printf("AWS IoT Manager Enabled: %v\n", *awsIoTManager)
+	if *awsIoTManager {
+		ai := *awsIoTManagerID
+		fmt.Printf("AWS IoT Manager ConnectorID: %s\n", ai)
+		fmt.Printf("AWS IoT Manager AccessKey: %s\n", *awsIoTManagerUser)
+		fmt.Printf("AWS IoT Manager SecretAccessKey: %s\n", *awsIoTManagerPass)
+		fmt.Printf("AWS IoT Manager Region: %s\n", *awsIoTManagerRegion)
+	}
+
 	fmt.Println("========== LAUNCHING AUXILIARY SERVICES ==========")
 	fmt.Println("Storage Engine")
 	fmt.Println(">> launching docker: Postgres ...")
 	pCleanup, storageConfig, err := postgres_test.RunPostgresDocker([]string{"ca", "alerts", "dmsmanager", "devicemanager", "cloudproxy"})
 	if err != nil {
-		log.Fatalf("could not launch postgres: %s", err)
+		log.Fatalf("could not launch Postgres: %s", err)
 	}
 	fmt.Println("Crypto Engines")
 	fmt.Println(">> launching docker: Hashicorp Vault ...")
 	vCleanup, vaultConfig, err := keyvaultkv2_test.RunHashicorpVaultDocker()
 	if err != nil {
-		log.Fatalf("could not launch postgres: %s", err)
+		log.Fatalf("could not launch Hashicorp Vault: %s", err)
+	}
+	fmt.Println(">> launching docker: AWS Platform (Secrets Manager + KMS) ...")
+	awsCleanup, awsCfg, err := awskmssm_test.RunAWSEmulationLocalStackDocker()
+	if err != nil {
+		log.Fatalf("could not launch AWS Platform: %s", err)
 	}
 
 	fmt.Println(">> launching docker: SoftHSM ...")
 	fmt.Println("Async Messaging Engine")
 	fmt.Println(">> launching docker: RabbitMQ ...")
+	rmqCleanup, rmqConfig, err := rabbitmq_test.RunRabbitMQDocker()
+	if err != nil {
+		log.Fatalf("could not launch RabbitMQ: %s", err)
+	}
+
 	fmt.Println("========== READY TO LAUNCH MONOLITHIC PKI ==========")
 
 	cleanup := func() {
@@ -55,6 +87,8 @@ func main() {
 
 		svcCleanup("Postgres", pCleanup)
 		svcCleanup("Hashicorp Vault", vCleanup)
+		svcCleanup("RabbitMQ", rmqCleanup)
+		svcCleanup("AWS-LocalStack", awsCleanup)
 	}
 
 	//capture future panics
@@ -87,7 +121,8 @@ func main() {
 
 	conf := config.MonolithicConfig{
 		BaseConfig: config.BaseConfig{
-			Logs: config.BaseConfigLogging{Level: config.Debug},
+			Logs:           config.BaseConfigLogging{Level: config.Debug},
+			AMQPConnection: *rmqConfig,
 		},
 		Domain:       "dev.lamassu.test",
 		GatewayPort:  8443,
@@ -102,6 +137,27 @@ func main() {
 					Metadata:          make(map[string]interface{}),
 				},
 			},
+			AWSKMSProvider: []config.AWSCryptoEngine{
+				config.AWSCryptoEngine{
+					AWSSDKConfig: *awsCfg,
+					ID:           "dockertest-localstack-kms",
+					Metadata:     make(map[string]interface{}),
+				},
+			},
+			AWSSecretsManagerProvider: []config.AWSCryptoEngine{
+				config.AWSCryptoEngine{
+					AWSSDKConfig: *awsCfg,
+					ID:           "dockertest-localstack-smngr",
+					Metadata:     make(map[string]interface{}),
+				},
+			},
+			GolangProvider: []config.GolangEngineConfig{
+				config.GolangEngineConfig{
+					ID:               "golang-1",
+					Metadata:         make(map[string]interface{}),
+					StorageDirectory: "/tmp/gotest",
+				},
+			},
 		},
 		CryptoMonitoring: config.CryptoMonitoring{
 			Enabled:   true,
@@ -111,6 +167,15 @@ func main() {
 			LogLevel: config.Info,
 			Provider: config.Postgres,
 			Postgres: *storageConfig,
+		},
+		AWSIoTManager: config.MonolithicAWSIoTManagerConfig{
+			Enabled:     *awsIoTManager,
+			ConnectorID: *awsIoTManagerID,
+			AWSSDKConfig: config.AWSSDKConfig{
+				AccessKeyID:     *awsIoTManagerUser,
+				SecretAccessKey: config.Password(*awsIoTManagerPass),
+				Region:          *awsIoTManagerRegion,
+			},
 		},
 	}
 
