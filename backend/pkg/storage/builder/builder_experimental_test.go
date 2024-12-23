6189f30e123d3d664af5710087c137eaac7eaa1b
Juanjo Rodriguez <jjrodriguez@lksnext.com>
2024-12-23 20:27:49 +0100

    chore: clean up codecov configuration (#196)

--- backend/pkg/storage/builder/builder_experimental_test.go
+++ backend/pkg/storage/builder/builder_experimental_test.go
@@ -1,5 +1,3 @@
-//go:build !experimental
-
 package builder
 
 import (
@@ -10,10 +8,10 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func TestBuildStorageEngineCouchDBMissing(t *testing.T) {
+func TestBuildStorageEngineUnknownMissing(t *testing.T) {
 	logger := log.WithField("test", "BuildStorageEngine_InvalidProvider")
 	conf := config.PluggableStorageEngine{
-		Provider: config.CouchDB,
+		Provider: "unkown",
 	}
 
 	// Call the BuildStorageEngine function
@@ -24,7 +22,7 @@ func TestBuildStorageEngineCouchDBMissing(t *testing.T) {
 		t.Error("expected an error, but got nil")
 	}
 
-	if err.Error() != fmt.Sprintf("no storage engine of type %s", config.CouchDB) {
+	if err.Error() != fmt.Sprintf("no storage engine of type %s", conf.Provider) {
 		t.Errorf("unexpected error: %s", err)
 	}
 }
