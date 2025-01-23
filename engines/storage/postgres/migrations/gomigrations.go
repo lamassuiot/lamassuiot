32ae58374a84ed9357187fe650a5d2fcdc72e88e
haritz <hsaizsierra@gmail.com>
2025-01-23 14:42:50 +0100

    add aws-related metadata migration to new schema defined in #166
    Signed-off-by: haritz <hsaizsierra@gmail.com>

--- engines/storage/postgres/migrations/gomigrations.go
+++ engines/storage/postgres/migrations/gomigrations.go
@@ -1,12 +1,15 @@
 package migrations
 
 import (
+	"github.com/lamassuiot/lamassuiot/engines/storage/postgres/v3/migrations/ca"
 	"github.com/lamassuiot/lamassuiot/engines/storage/postgres/v3/migrations/dmsmanager"
 )
 
 // the order of the migrations is NOT IMPORTANT. It is only necessary to register them all
 func RegisterGoMigrations(dbname string) {
 	switch dbname {
+	case "ca":
+		ca.Register_20250123125500_ca_aws_metadata()
 	case "dmsmanager":
 		dmsmanager.Register_20241230124809_serverkeygen_revokereenroll()
 	}
