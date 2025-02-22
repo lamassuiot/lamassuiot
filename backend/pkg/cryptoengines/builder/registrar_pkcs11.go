60ffc460ddf337f746f3c79f7f700164c7991765
Juan Jose Rodriguez <jjrodriguez@lksnext.com>
2025-02-22 18:47:34 +0100

    chore: modularize engine registration with build tags to favour custom builds
    Signed-off-by: Juan Jose Rodriguez <jjrodriguez@lksnext.com>

---
+++ backend/pkg/cryptoengines/builder/registrar_pkcs11.go
@@ -0,0 +1,15 @@
+//go:build !nopkcs11
+
+package builder
+
+import (
+	"github.com/lamassuiot/lamassuiot/engines/crypto/pkcs11/v3"
+	pkcs11_subsystem "github.com/lamassuiot/lamassuiot/engines/crypto/pkcs11/v3/subsystem"
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	log.Info("Registering VaultKV crypto engines")
+	pkcs11.Register()
+	pkcs11_subsystem.Register()
+}
