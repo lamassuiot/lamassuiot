1695f4a08605a5b730d1ead43f6a3ff0db45631f
Jon Galipienzo <jgalipienzo@lksnext.com>
2025-06-03 10:45:54 +0200

    perf: improve performance  by only joining when CN filter is present
    Signed-off-by: Jon Galipienzo <jgalipienzo@lksnext.com>

--- engines/storage/postgres/castore.go
+++ engines/storage/postgres/castore.go
@@ -10,6 +10,7 @@ import (
 )
 
 const caDBName = "ca_certificates"
+const caJoinCaCertificatesAndCertificates = "JOIN certificates ON ca_certificates.serial_number = certificates.serial_number"
 
 type PostgresCAStore struct {
 	db      *gorm.DB
@@ -34,26 +35,34 @@ func (db *PostgresCAStore) Count(ctx context.Context) (int, error) {
 
 func (db *PostgresCAStore) CountByEngine(ctx context.Context, engineID string) (int, error) {
 	return db.querier.Count(ctx, []gormExtraOps{
-		{query: "certificates.engine_id = ? ", additionalWhere: []any{engineID}, joins: []string{"JOIN certificates ON ca_certificates.serial_number = certificates.serial_number"}},
+		{query: "certificates.engine_id = ? ", additionalWhere: []any{engineID}, joins: []string{caJoinCaCertificatesAndCertificates}},
 	})
 }
 
 func (db *PostgresCAStore) CountByStatus(ctx context.Context, status models.CertificateStatus) (int, error) {
 	return db.querier.Count(ctx, []gormExtraOps{
-		{query: "certificates.status = ? ", additionalWhere: []any{status}, joins: []string{"JOIN certificates ON ca_certificates.serial_number = certificates.serial_number"}},
+		{query: "certificates.status = ? ", additionalWhere: []any{status}, joins: []string{caJoinCaCertificatesAndCertificates}},
 	})
 }
 
 func (db *PostgresCAStore) SelectByType(ctx context.Context, CAType models.CertificateType, req storage.StorageListRequest[models.CACertificate]) (string, error) {
 	opts := []gormExtraOps{
-		{query: "certificates.type = ? ", additionalWhere: []any{CAType}, joins: []string{"JOIN certificates ON ca_certificates.serial_number = certificates.serial_number"}},
+		{query: "certificates.type = ? ", additionalWhere: []any{CAType}, joins: []string{caJoinCaCertificatesAndCertificates}},
 	}
 	return db.querier.SelectAll(ctx, req.QueryParams, opts, req.ExhaustiveRun, req.ApplyFunc)
 }
 
 func (db *PostgresCAStore) SelectAll(ctx context.Context, req storage.StorageListRequest[models.CACertificate]) (string, error) {
-	opts := []gormExtraOps{
-		{joins: []string{"JOIN certificates ON ca_certificates.serial_number = certificates.serial_number"}},
+	opts := []gormExtraOps{}
+	if req.QueryParams != nil {
+		for _, filter := range req.QueryParams.Filters {
+			if filter.Field == "subject.common_name" {
+				opts = []gormExtraOps{
+					{joins: []string{caJoinCaCertificatesAndCertificates}},
+				}
+				break
+			}
+		}
 	}
 
 	return db.querier.SelectAll(ctx, req.QueryParams, opts, req.ExhaustiveRun, req.ApplyFunc)
@@ -61,7 +70,7 @@ func (db *PostgresCAStore) SelectAll(ctx context.Context, req storage.StorageLis
 
 func (db *PostgresCAStore) SelectByCommonName(ctx context.Context, commonName string, req storage.StorageListRequest[models.CACertificate]) (string, error) {
 	return db.querier.SelectAll(ctx, req.QueryParams, []gormExtraOps{
-		{query: "certificates.subject_common_name = ? ", additionalWhere: []any{commonName}, joins: []string{"JOIN certificates ON ca_certificates.serial_number = certificates.serial_number"}},
+		{query: "certificates.subject_common_name = ? ", additionalWhere: []any{commonName}, joins: []string{caJoinCaCertificatesAndCertificates}},
 	}, req.ExhaustiveRun, req.ApplyFunc)
 }
 
@@ -72,43 +81,51 @@ func (db *PostgresCAStore) SelectExistsBySerialNumber(ctx context.Context, seria
 
 func (db *PostgresCAStore) SelectByParentCA(ctx context.Context, parentCAID string, req storage.StorageListRequest[models.CACertificate]) (string, error) {
 	return db.querier.SelectAll(ctx, req.QueryParams, []gormExtraOps{
-		{query: "certificates.issuer_meta_id = ? AND id != ?", additionalWhere: []any{parentCAID, parentCAID}, joins: []string{"JOIN certificates ON ca_certificates.serial_number = certificates.serial_number"}},
+		{query: "certificates.issuer_meta_id = ? AND id != ?", additionalWhere: []any{parentCAID, parentCAID}, joins: []string{caJoinCaCertificatesAndCertificates}},
 	}, req.ExhaustiveRun, req.ApplyFunc)
 }
 
 func (db *PostgresCAStore) SelectBySubjectAndSubjectKeyID(ctx context.Context, sub models.Subject, skid string, req storage.StorageListRequest[models.CACertificate]) (string, error) {
 	return db.querier.SelectAll(ctx, req.QueryParams, []gormExtraOps{
-		{query: "certificates.subject_common_name = ? AND " +
-			"certificates.subject_organization = ? AND " +
-			"certificates.subject_organization_unit = ? AND " +
-			"certificates.subject_country = ? AND " +
-			"certificates.subject_state = ? AND " +
-			"certificates.subject_locality = ? AND " +
-			"subject_key_id = ?", additionalWhere: []any{sub.CommonName,
-			sub.Organization,
-			sub.OrganizationUnit,
-			sub.Country,
-			sub.State,
-			sub.Locality,
-			skid}, joins: []string{"JOIN certificates ON ca_certificates.serial_number = certificates.serial_number"}},
+		{
+			query: "certificates.subject_common_name = ? AND " +
+				"certificates.subject_organization = ? AND " +
+				"certificates.subject_organization_unit = ? AND " +
+				"certificates.subject_country = ? AND " +
+				"certificates.subject_state = ? AND " +
+				"certificates.subject_locality = ? AND " +
+				"subject_key_id = ?",
+			additionalWhere: []any{sub.CommonName,
+				sub.Organization,
+				sub.OrganizationUnit,
+				sub.Country,
+				sub.State,
+				sub.Locality,
+				skid},
+			joins: []string{caJoinCaCertificatesAndCertificates},
+		},
 	}, req.ExhaustiveRun, req.ApplyFunc)
 }
 
 func (db *PostgresCAStore) SelectByIssuerAndAuthorityKeyID(ctx context.Context, iss models.Subject, akid string, req storage.StorageListRequest[models.CACertificate]) (string, error) {
 	return db.querier.SelectAll(ctx, req.QueryParams, []gormExtraOps{
-		{query: "certificates.issuer_common_name = ? AND " +
-			"certificates.issuer_organization = ? AND " +
-			"certificates.issuer_organization_unit = ? AND " +
-			"certificates.issuer_country = ? AND " +
-			"certificates.issuer_state = ? AND " +
-			"certificates.issuer_locality = ? AND " +
-			"authority_key_id = ?", additionalWhere: []any{iss.CommonName,
-			iss.Organization,
-			iss.OrganizationUnit,
-			iss.Country,
-			iss.State,
-			iss.Locality,
-			akid}, joins: []string{"JOIN certificates ON ca_certificates.serial_number = certificates.serial_number"}},
+		{
+			query: "certificates.issuer_common_name = ? AND " +
+				"certificates.issuer_organization = ? AND " +
+				"certificates.issuer_organization_unit = ? AND " +
+				"certificates.issuer_country = ? AND " +
+				"certificates.issuer_state = ? AND " +
+				"certificates.issuer_locality = ? AND " +
+				"authority_key_id = ?",
+			additionalWhere: []any{iss.CommonName,
+				iss.Organization,
+				iss.OrganizationUnit,
+				iss.Country,
+				iss.State,
+				iss.Locality,
+				akid},
+			joins: []string{caJoinCaCertificatesAndCertificates},
+		},
 	}, req.ExhaustiveRun, req.ApplyFunc)
 }
 
