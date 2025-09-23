320ee4c48b1b83e8290085ee77ae316689a7b377
David Gilarranz Ortega <dgilarranz@ikerlan.es>
2025-09-23 15:51:35 +0200

    Added the ImportMLDSAPrivateKey function to fulfill the interface requirements
    Signed-off-by: Haritz S. Sierra <hsaizsierra@gmail.com>

--- engines/crypto/pkcs11/pkcs11.go
+++ engines/crypto/pkcs11/pkcs11.go
@@ -10,7 +10,6 @@ import (
 	"crypto/elliptic"
 	"crypto/rsa"
 	"crypto/x509"
-	"errors"
 	"fmt"
 	"os"
 
@@ -54,7 +53,7 @@ func NewPKCS11Engine(logger *logrus.Entry, conf config.CryptoEngineConfigAdapter
 	instance, err := crypto11.Configure(config)
 	if err != nil {
 		lPkcs11.Errorf("could not configure pkcs11 module: %s", err)
-		return nil, errors.New("could not configure driver")
+		return nil, fmt.Errorf("could not configure driver")
 	}
 
 	pkcs11ProviderContext := pkcs11.New(conf.Config.ModulePath)
@@ -258,8 +257,9 @@ func (hsmContext *pkcs11EngineContext) CreateECDSAPrivateKey(ctx context.Context
 	return keyID, renamedSigner, nil
 }
 
+// TODO -> Add implementation (if posible)
 func (hsmContext *pkcs11EngineContext) CreateMLDSAPrivateKey(dimensions int) (string, crypto.Signer, error) {
-	return "", nil, errors.New("pkcs11: unsupported key type (ML-DSA")
+	return "", nil, fmt.Errorf("pkcs11: unsupported key type (ML-DSA")
 }
 
 // define a constant for the key ID using ints and iota
@@ -316,6 +316,11 @@ func (hsmContext *pkcs11EngineContext) ImportECDSAPrivateKey(key *ecdsa.PrivateK
 	return "", nil, fmt.Errorf("TODO")
 }
 
+// TODO -> Add implementation (if posible)
+func (hsmContext *pkcs11EngineContext) ImportMLDSAPrivateKey(key crypto.Signer) (string, crypto.Signer, error) {
+	return "", nil, fmt.Errorf("pkcs11: unsupported key type (ML-DSA")
+}
+
 func (hsmContext *pkcs11EngineContext) DeleteKey(keyID string) error {
 	return fmt.Errorf("TODO")
 }
@@ -340,5 +345,5 @@ func findKeyWithAttributes(ctx pkcs11.Ctx, sh pkcs11.SessionHandle, template []*
 		return &newhandles[0], nil
 	}
 
-	return nil, errors.New("object not found")
+	return nil, fmt.Errorf("object not found")
 }
