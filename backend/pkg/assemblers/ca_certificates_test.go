5d2181ace97538cef0abe33ec9b5aa80a2c4c60f
hsaizsierra <hsaizsierra@gmail.com>
2025-09-26 16:05:57 +0200

    adding kms db to tests
    Signed-off-by: hsaizsierra <hsaizsierra@gmail.com>

--- backend/pkg/assemblers/ca_certificates_test.go
+++ backend/pkg/assemblers/ca_certificates_test.go
@@ -184,7 +184,7 @@ func TestDeleteCertificateService(t *testing.T) {
 }
 
 func TestSignCertificateWithDefaultProfile(t *testing.T) {
-	serverTest, err := TestServiceBuilder{}.WithDatabase("ca").Build(t)
+	serverTest, err := TestServiceBuilder{}.WithDatabase("ca", "kms").Build(t)
 	if err != nil {
 		t.Fatalf("could not create CA test server: %s", err)
 	}
