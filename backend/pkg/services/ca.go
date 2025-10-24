fd2e9b847620c9533565d2ec5334c0dd9c964a8a
David Gilarranz Ortega <dgilarranz@ikerlan.es>
2025-10-24 10:42:25 +0200

    CA: removed old metadata so as to conform to the new specification
    Signed-off-by: David Gilarranz Ortega <dgilarranz@ikerlan.es>

--- backend/pkg/services/ca.go
+++ backend/pkg/services/ca.go
@@ -784,8 +784,6 @@ func (svc *CAServiceBackend) createChameleonCA(ctx context.Context, input servic
 			"keyinfo": input.InnerKeyMetadata.Type.String(),
 		},
 	}
-	input.CreateCAInput.Metadata["delta_skid"] = deltaSkid
-	input.CreateCAInput.Metadata["delta_akid"] = deltaAkid
 
 	caCert := models.CACertificate{
 		ID:         caID,
