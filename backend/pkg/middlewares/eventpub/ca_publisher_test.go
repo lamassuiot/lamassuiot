5c96bffa9dfbc1c3af94b0186e8765a221d51fcf
Juanjo Rodriguez <jjrodriguez@lksnext.com>
2025-11-17 12:50:42 +0100

    feat: Add audit event publisher tests for CA, CRL, devices, and DMS functionalities
    Signed-off-by: Juanjo Rodriguez <jjrodriguez@lksnext.com>

--- backend/pkg/middlewares/eventpub/ca_publisher_test.go
+++ backend/pkg/middlewares/eventpub/ca_publisher_test.go
@@ -227,6 +227,84 @@ func TestCAEventPublisher(t *testing.T) {
 					})
 			},
 		},
+		{
+			name: "ReissueCA with errors - Not fire event",
+			test: func(t *testing.T) {
+				withErrors(t, "ReissueCA", services.ReissueCAInput{}, models.EventReissueCAKey, &models.CACertificate{},
+					func(mockCAService *svcmock.MockCAService) {
+						mockCAService.On("GetCAByID", mock.Anything, mock.Anything).Return(&models.CACertificate{}, nil)
+					})
+			},
+		},
+		{
+			name: "ReissueCA without errors - fire event",
+			test: func(t *testing.T) {
+				withoutErrors(t, "ReissueCA", services.ReissueCAInput{}, models.EventReissueCAKey, &models.CACertificate{},
+					func(mockCAService *svcmock.MockCAService) {
+						mockCAService.On("GetCAByID", mock.Anything, mock.Anything).Return(&models.CACertificate{}, nil)
+					})
+			},
+		},
+		{
+			name: "CreateKey with errors - Not fire event",
+			test: func(t *testing.T) {
+				withErrors(t, "CreateKey", services.CreateKeyInput{}, models.EventCreateKMSKey, &models.Key{})
+			},
+		},
+		{
+			name: "CreateKey without errors - fire event",
+			test: func(t *testing.T) {
+				withoutErrors(t, "CreateKey", services.CreateKeyInput{}, models.EventCreateKMSKey, &models.Key{})
+			},
+		},
+		{
+			name: "DeleteKeyByID with errors - Not fire event",
+			test: func(t *testing.T) {
+				withErrorsSingleResult(t, "DeleteKeyByID", services.GetByIDInput{}, models.EventDeleteKMSKey)
+			},
+		},
+		{
+			name: "DeleteKeyByID without errors - fire event",
+			test: func(t *testing.T) {
+				withoutErrorsSingleResult(t, "DeleteKeyByID", services.GetByIDInput{}, models.EventDeleteKMSKey)
+			},
+		},
+		{
+			name: "SignMessage with errors - Not fire event",
+			test: func(t *testing.T) {
+				withErrors(t, "SignMessage", services.SignMessageInput{}, models.EventSignMessageKMSKey, &models.MessageSignature{})
+			},
+		},
+		{
+			name: "SignMessage without errors - fire event",
+			test: func(t *testing.T) {
+				withoutErrors(t, "SignMessage", services.SignMessageInput{}, models.EventSignMessageKMSKey, &models.MessageSignature{})
+			},
+		},
+		{
+			name: "VerifySignature with errors - Not fire event",
+			test: func(t *testing.T) {
+				withErrors(t, "VerifySignature", services.VerifySignInput{}, models.EventVerifySignatureKMSKey, &models.MessageValidation{})
+			},
+		},
+		{
+			name: "VerifySignature without errors - fire event",
+			test: func(t *testing.T) {
+				withoutErrors(t, "VerifySignature", services.VerifySignInput{}, models.EventVerifySignatureKMSKey, &models.MessageValidation{})
+			},
+		},
+		{
+			name: "ImportKey with errors - Not fire event",
+			test: func(t *testing.T) {
+				withErrors(t, "ImportKey", services.ImportKeyInput{}, models.EventImportKMSKey, &models.Key{})
+			},
+		},
+		{
+			name: "ImportKey without errors - fire event",
+			test: func(t *testing.T) {
+				withoutErrors(t, "ImportKey", services.ImportKeyInput{}, models.EventImportKMSKey, &models.Key{})
+			},
+		},
 	}
 
 	for _, tc := range testcases {
