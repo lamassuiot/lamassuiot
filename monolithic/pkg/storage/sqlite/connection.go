5a3c6a7be8615f83f40f4bf01392c509ea2fa62b
Juanjo Rodriguez <jjrodriguez@lksnext.com>
2025-12-17 18:46:48 +0100

    fix: case-insensitive LIKE replacement in SQLite
    Signed-off-by: Juanjo Rodriguez <jjrodriguez@lksnext.com>

--- monolithic/pkg/storage/sqlite/connection.go
+++ monolithic/pkg/storage/sqlite/connection.go
@@ -1,9 +1,6 @@
 package sqlite
 
 import (
-	"context"
-	"database/sql"
-	"strings"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -12,31 +9,6 @@ import (
 	"gorm.io/gorm/logger"
 )
 
-// sqliteConnWrapper wraps the SQL connection to replace ILIKE with LIKE
-type sqliteConnWrapper struct {
-	gorm.ConnPool
-}
-
-func (c *sqliteConnWrapper) ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error) {
-	query = strings.ReplaceAll(query, " ILIKE ", " LIKE ")
-	return c.ConnPool.ExecContext(ctx, query, args...)
-}
-
-func (c *sqliteConnWrapper) QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error) {
-	query = strings.ReplaceAll(query, " ILIKE ", " LIKE ")
-	return c.ConnPool.QueryContext(ctx, query, args...)
-}
-
-func (c *sqliteConnWrapper) QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row {
-	query = strings.ReplaceAll(query, " ILIKE ", " LIKE ")
-	return c.ConnPool.QueryRowContext(ctx, query, args...)
-}
-
-func (c *sqliteConnWrapper) PrepareContext(ctx context.Context, query string) (*sql.Stmt, error) {
-	query = strings.ReplaceAll(query, " ILIKE ", " LIKE ")
-	return c.ConnPool.PrepareContext(ctx, query)
-}
-
 func CreateSQLiteDBConnection(log *logrus.Entry, dbPath string) (*gorm.DB, error) {
 	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Silent),
@@ -44,9 +16,6 @@ func CreateSQLiteDBConnection(log *logrus.Entry, dbPath string) (*gorm.DB, error
 	if err != nil {
 		return nil, err
 	}
-
-	// Wrap ConnPool to replace ILIKE with LIKE
-	db.ConnPool = &sqliteConnWrapper{ConnPool: db.ConnPool}
 
 	// Get underlying SQL DB for connection pool configuration
 	sqlDB, err := db.DB()
